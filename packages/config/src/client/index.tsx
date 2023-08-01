58946a982cdea677aff28f5a6342ca5e03267f42
RedCrafter07 <gh@r07.dev>
2023-08-01 11:56:07 +0200

    Add some homepage updates

--- packages/config/src/client/index.tsx
+++ packages/config/src/client/index.tsx
@@ -1,13 +1,24 @@
-import '@fontsource/figtree';
-import './index.css';
-import ReactDOM from 'react-dom/client';
 import React from 'react';
+import ReactDOM from 'react-dom/client';
+import '@fontsource/figtree';
+import '@fontsource/figtree/900.css';
+import './index.css';
 
 function App() {
 	return (
-		<>
-			<h1>Hi</h1>
-		</>
+		<div className='bg-zinc-800 text-zinc-100 min-h-screen'>
+			<div className='container mx-auto p-2'>
+				<h1 className='text-3xl'>Welcome to the RedDeploy Configuration!</h1>
+
+				<h3 className='text-xl'>1. Access URL</h3>
+				<p>
+					Let's configure your Access URL first. You should choose a domain
+					name, like <code>rd.example.com</code>.
+				</p>
+
+				<input type='text' name='domain' placeholder='rd.example.com' />
+			</div>
+		</div>
 	);
 }
 
