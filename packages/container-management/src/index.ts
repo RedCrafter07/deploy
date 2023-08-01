264ffeb52432db404260ea87557268020cfaee1c
RedCrafter07 <gh@r07.dev>
2023-08-01 19:45:58 +0200

    Add io server

---
+++ packages/container-management/src/index.ts
@@ -0,0 +1,12 @@
+import { Server } from 'socket.io';
+
+const io = new Server({
+	transports: ['websocket'],
+});
+
+io.on('connect', (socket) => {
+	console.log('A socket connected!');
+});
+
+io.listen(8080);
+console.log('CM socket listening');
