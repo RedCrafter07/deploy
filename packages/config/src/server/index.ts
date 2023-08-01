5b22a75f9a97c04a889a051fea40feb417625581
RedCrafter07 <gh@r07.dev>
2023-08-01 19:07:06 +0200

    Finish installation

--- packages/config/src/server/index.ts
+++ packages/config/src/server/index.ts
@@ -44,6 +44,108 @@ let blockConfig = false;
 
 			socket.emit('step', 'Pulling images... (4/4)');
 			await docker.pull('ghcr.io/RedCrafter07/deploy/proxy');
+
+			socket.emit('step', 'Creating network... (1/2)');
+			await docker.createNetwork({
+				Name: 'reddeploy',
+				Attachable: true,
+			});
+
+			socket.emit('step', 'Creating network... (2/2)');
+			await docker.createNetwork({
+				Name: 'reddeploy-proxy',
+				Attachable: true,
+			});
+
+			socket.emit('step', 'Creating volumes... (1/2)');
+			await docker.createVolume({
+				Name: 'reddeploy-mongo',
+			});
+
+			socket.emit('step', 'Creating volumes... (2/2)');
+			await docker.createVolume({
+				Name: 'reddeploy-cm-cache',
+			});
+
+			socket.emit('step', 'Creating containers... (1/4)');
+			await docker.createContainer({
+				Image: 'mongo:4.2.17',
+				name: 'reddeploy-mongo',
+				Env: [
+					'MONGO_INITDB_ROOT_USERNAME=root',
+					'MONGO_INITDB_ROOT_PASSWORD=reddeploy',
+				],
+				HostConfig: {
+					NetworkMode: 'reddeploy',
+				},
+				Volumes: {
+					'/data/db': {
+						Name: 'reddeploy-mongo',
+					},
+				},
+			});
+
+			socket.emit('step', 'Creating containers... (2/4)');
+			await docker.createContainer({
+				Image: 'ghcr.io/RedCrafter07/deploy/cm',
+				name: 'reddeploy-cm',
+				Env: [
+					'CM_MONGO_HOST=reddeploy-mongo',
+					'CM_MONGO_PORT=27017',
+					'CM_MONGO_USER=root',
+					'CM_MONGO_PASS=reddeploy',
+					'CM_MONGO_DB=reddeploy',
+					'CM_PORT=8080',
+				],
+				HostConfig: {
+					NetworkMode: 'reddeploy',
+					Binds: [
+						'reddeploy-cm-cache:/app/cache',
+						'/var/run/docker.sock:/var/run/docker.sock',
+					],
+				},
+			});
+
+			socket.emit('step', 'Creating containers... (3/4)');
+			await docker.createContainer({
+				Image: 'ghcr.io/RedCrafter07/deploy/web',
+				name: 'reddeploy-web',
+				Env: ['WEB_CM_HOST=reddeploy-cm', 'WEB_CM_PORT=8080', 'WEB_PORT=80'],
+				HostConfig: {
+					NetworkMode: 'reddeploy',
+				},
+			});
+
+			socket.emit('step', 'Creating containers... (4/4)');
+			await docker.createContainer({
+				Image: 'ghcr.io/RedCrafter07/deploy/proxy',
+				name: 'reddeploy-proxy',
+				Env: [
+					'PROXY_WEB_HOST=reddeploy-web',
+					'PROXY_WEB_PORT=80',
+					'PROXY_PORT=80',
+				],
+				HostConfig: {
+					NetworkMode: 'reddeploy-proxy',
+				},
+			});
+
+			socket.emit('step', 'Starting containers... (1/4)');
+			await docker.getContainer('reddeploy-mongo').start();
+
+			socket.emit('step', 'Starting containers... (2/4)');
+			await docker.getContainer('reddeploy-cm').start();
+
+			socket.emit('step', 'Starting containers... (3/4)');
+			await docker.getContainer('reddeploy-web').start();
+
+			socket.emit('step', 'Starting containers... (4/4)');
+			await docker.getContainer('reddeploy-proxy').start();
+
+			// TODO: Import config to database
+
+			socket.emit('step', 'Done!');
+			socket.emit('view', 'done');
 		});
 	});
 
