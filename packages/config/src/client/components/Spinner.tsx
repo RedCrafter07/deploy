c2db59921f82bb1d8965dc257df6ee5add6ec5ae
RedCrafter07 <gh@r07.dev>
2023-08-01 18:43:01 +0200

    Add spinner to config container

---
+++ packages/config/src/client/components/Spinner.tsx
@@ -0,0 +1,10 @@
+export default function Spinner() {
+	return (
+		<div
+			className='w-full aspect-square border-4 rounded-full animate-spin'
+			style={{
+				borderColor: '#fff transparent #fff transparent',
+			}}
+		/>
+	);
+}
