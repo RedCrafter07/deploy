e6a8fb1c4f60122f91dfba02fee8c18ade562fcc
RedCrafter07 <gh@r07.dev>
2023-08-01 19:46:05 +0200

    Connect to CM Socket

--- packages/web/src/server/index.ts
+++ packages/web/src/server/index.ts
@@ -1,8 +1,14 @@
 import express from 'express';
+import { io } from 'socket.io-client';
 
 const app = express();
 
 const cmURL = `${process.env.WEB_CM_HOST}:${process.env.WEB_CM_PORT}`;
+const cmSocket = io(`http://${cmURL}`, {
+	transports: ['websocket'],
+	reconnection: true,
+});
+cmSocket.connect();
 
 app.get('*', (req, res) => {
 	res.send('Coming soon!');
