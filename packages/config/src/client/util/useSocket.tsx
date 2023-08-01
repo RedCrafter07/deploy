be693172a412b96c5bffb4eaf3cbe9d9375b036d
RedCrafter07 <gh@r07.dev>
2023-08-01 12:21:00 +0200

    Add useSocket function

---
+++ packages/config/src/client/util/useSocket.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import { io } from 'socket.io-client';
+
+export default function useSocket() {
+	const [socket] = useState(
+		io(window.location.origin, {
+			transports: ['websocket'],
+			reconnection: true,
+		}),
+	);
+
+	return socket;
+}
