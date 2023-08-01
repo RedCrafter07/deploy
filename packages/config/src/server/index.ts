f13e3f826c6648c0a59d8e76615c3cb719aef96f
RedCrafter07 <gh@r07.dev>
2023-08-01 11:56:29 +0200

    Use async function & init data dir

--- packages/config/src/server/index.ts
+++ packages/config/src/server/index.ts
@@ -1,17 +1,24 @@
 import express from 'express';
+import { mkdir } from 'fs/promises';
 import path from 'path';
 
-const app = express();
+(async () => {
+	const app = express();
 
-app.use('/.rd', express.static(path.join(__dirname, '..', 'client')));
+	await mkdir(path.join(__dirname, '..', '..', 'data'), { recursive: true });
 
-app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname, '..', '..', 'src', 'client', 'index.html'));
-});
-app.get('*', (req, res) => {
-	res.redirect('/');
-});
+	app.use('/.rd', express.static(path.join(__dirname, '..', 'client')));
 
-app.listen(9272, () => {
-	console.log('Config interface running on port 9272');
-});
+	app.get('/', (req, res) => {
+		res.sendFile(
+			path.join(__dirname, '..', '..', 'src', 'client', 'index.html'),
+		);
+	});
+	app.get('*', (req, res) => {
+		res.redirect('/');
+	});
+
+	app.listen(9272, () => {
+		console.log('Config interface running on port 9272');
+	});
+})();
