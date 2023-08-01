bf2a99326ab4b3a114273b23cc60b29be46684e0
RedCrafter07 <gh@r07.dev>
2023-08-01 12:21:58 +0200

    Add socket.io

--- packages/config/src/server/index.ts
+++ packages/config/src/server/index.ts
@@ -1,9 +1,19 @@
 import express from 'express';
 import { mkdir } from 'fs/promises';
+import { createServer } from 'http';
 import path from 'path';
+import { Server as SocketServer } from 'socket.io';
 
 (async () => {
 	const app = express();
+	const server = createServer(app);
+	const io = new SocketServer(server, {
+		transports: ['websocket'],
+	});
+
+	io.on('connect', (socket) => {
+		socket.emit('hello', 'world');
+	});
 
 	await mkdir(path.join(__dirname, '..', '..', 'data'), { recursive: true });
 
@@ -18,7 +28,7 @@ import path from 'path';
 		res.redirect('/');
 	});
 
-	app.listen(9272, () => {
+	server.listen(9272, () => {
 		console.log('Config interface running on port 9272');
 	});
 })();
