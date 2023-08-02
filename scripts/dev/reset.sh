34ea10eebf4d0a47d772ca60b487fbb4a8b7aa3f
RedCrafter07 <gh@r07.dev>
2023-08-02 22:28:14 +0200

    Force prune
    -> No confirmation required

--- scripts/dev/reset.sh
+++ scripts/dev/reset.sh
@@ -1,6 +1,6 @@
 docker stop reddeploy-mongo reddeploy-cm reddeploy-web reddeploy-scm
 docker rm reddeploy-mongo reddeploy-cm reddeploy-web reddeploy-scm
 
-docker volume prune
+docker volume prune -f
 
-docker network prune
+docker network prune -f
