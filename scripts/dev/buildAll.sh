63eca5f1c1844cca1c3594a380a02f19ee866582
RedCrafter07 <gh@r07.dev>
2023-08-02 18:38:06 +0200

    Change image names

--- scripts/dev/buildAll.sh
+++ scripts/dev/buildAll.sh
@@ -1,8 +1,8 @@
 cd packages/
 
 cd config/ && docker build -t reddeploy/config:latest -f Dockerfile.dev . && cd ..
-cd container-management/ && docker build -t reddeploy/container-management:latest . && cd ..
-cd system-container-management/ && docker build -t reddeploy/system-container-management:latest . && cd ..
+cd container-management/ && docker build -t reddeploy/cm:latest . && cd ..
+cd system-container-management/ && docker build -t reddeploy/scm:latest . && cd ..
 cd web/ && docker build -t reddeploy/web:latest . && cd ..
 
 cd ../
