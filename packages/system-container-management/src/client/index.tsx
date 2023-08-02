3b6d44ffd01966c5b021cda3edaf8ef26ba28e80
RedCrafter07 <gh@r07.dev>
2023-08-02 23:35:12 +0200

    Log connection

--- packages/system-container-management/src/client/index.tsx
+++ packages/system-container-management/src/client/index.tsx
@@ -2,11 +2,20 @@ import { createRoot } from 'react-dom/client';
 import useSocket from './util/useSocket';
 import '@fontsource/figtree';
 import './index.css';
+import { useEffect } from 'react';
 
 createRoot(document.getElementById('root')!).render(<App />);
 
 function App() {
 	const socket = useSocket();
+
+	useEffect(() => {
+		socket.on('connect', () => {
+			console.log('Connected to server');
+		});
+
+		socket.connect();
+	}, []);
 
 	return (
 		<div className='min-h-screen bg-zinc-800 text-zinc-100'>
