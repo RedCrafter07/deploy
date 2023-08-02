54557789853435a81ed8fa34bf1eea1661c40c85
RedCrafter07 <gh@r07.dev>
2023-08-02 23:30:13 +0200

    Import styles & fonts

--- packages/system-container-management/src/client/index.tsx
+++ packages/system-container-management/src/client/index.tsx
@@ -1,5 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import useSocket from './util/useSocket';
+import '@fontsource/figtree';
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(<App />);
 
