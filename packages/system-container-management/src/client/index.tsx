f02971068171433f0bef2b46f2df6d3b79f93efc
RedCrafter07 <gh@r07.dev>
2023-08-02 22:57:11 +0200

    Use useSocket function

--- packages/system-container-management/src/client/index.tsx
+++ packages/system-container-management/src/client/index.tsx
@@ -1,7 +1,10 @@
 import { createRoot } from 'react-dom/client';
+import useSocket from './util/useSocket';
 
 createRoot(document.getElementById('root')!).render(<App />);
 
 function App() {
+	const socket = useSocket();
+
 	return <>Hello</>;
 }
