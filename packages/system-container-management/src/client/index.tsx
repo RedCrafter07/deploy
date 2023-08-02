8d1493bb1d12923e82921aa124f875ecc1fdcb65
RedCrafter07 <gh@r07.dev>
2023-08-03 00:02:53 +0200

    Add login card

--- packages/system-container-management/src/client/index.tsx
+++ packages/system-container-management/src/client/index.tsx
@@ -14,13 +14,54 @@ function App() {
 			console.log('Connected to server');
 		});
 
+		socket.on('login', (success: boolean) => {
+			console.log('Login', success);
+		});
+
 		socket.connect();
 	}, []);
 
 	return (
 		<div className='min-h-screen bg-zinc-800 text-zinc-100'>
-			<div className='container mx-auto p-2'>
-				<h1 className='text-3xl'>Hello</h1>
+			<div className='h-screen grid place-items-center'>
+				<div className='p-4 border border-white border-opacity-10'>
+					<h1 className='text-4xl font-bold text-center'>
+						RedDeploy System Container Management
+					</h1>
+					<h3 className='text-xl'>Please log in</h3>
+					<form
+						className='flex flex-col gap-2 mt-4'
+						onSubmit={(e) => {
+							e.preventDefault();
+
+							const data = new FormData(e.target as HTMLFormElement);
+
+							const username = data.get('username') as string;
+							const password = data.get('password') as string;
+
+							if (username.length <= 0 || password.length <= 0) return;
+
+							socket.emit('login', { username, password });
+						}}
+					>
+						<label className='flex flex-col gap-1'>
+							<span>Username</span>
+							<input type='text' className='input' required />
+						</label>
+						<label className='flex flex-col gap-1'>
+							<span>Password</span>
+							<input
+								type='password'
+								name='password'
+								className='input'
+								required
+							/>
+						</label>
+						<button className='p-2 bg-zinc-700 rounded-md w-full'>
+							Log in
+						</button>
+					</form>
+				</div>
 			</div>
 		</div>
 	);
