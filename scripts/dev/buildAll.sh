2ad1d53a0c294f6525798e66de640c14b98fe93e
RedCrafter07 <gh@r07.dev>
2023-08-02 19:00:08 +0200

    Use Dockerfile.dev

--- scripts/dev/buildAll.sh
+++ scripts/dev/buildAll.sh
@@ -2,7 +2,7 @@ cd packages/
 
 cd config/ && docker build -t reddeploy/config:latest -f Dockerfile.dev . && cd ..
 cd container-management/ && docker build -t reddeploy/cm:latest . && cd ..
-cd system-container-management/ && docker build -t reddeploy/scm:latest . && cd ..
+cd system-container-management/ && docker build -t reddeploy/scm:latest -f Dockerfile.dev . && cd ..
 cd web/ && docker build -t reddeploy/web:latest . && cd ..
 
 cd ../
