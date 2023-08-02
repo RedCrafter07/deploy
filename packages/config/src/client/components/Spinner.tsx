cc006bdf08c34bbd42a9387aa2d8495383f7f79e
RedCrafter07 <gh@r07.dev>
2023-08-02 11:21:01 +0200

    Change spinner styles

--- packages/config/src/client/components/Spinner.tsx
+++ packages/config/src/client/components/Spinner.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Spinner() {
 	return (
 		<div
-			className='w-full aspect-square border-4 rounded-full animate-spin'
+			className='w-full aspect-square border-8 rounded-full animate-spin'
 			style={{
 				borderColor: '#fff transparent #fff transparent',
 			}}
