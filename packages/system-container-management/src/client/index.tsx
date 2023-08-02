120830c06317af5d63ffc794781f098410f1adb8
RedCrafter07 <gh@r07.dev>
2023-08-02 23:30:54 +0200

    Add better styles

--- packages/system-container-management/src/client/index.tsx
+++ packages/system-container-management/src/client/index.tsx
@@ -8,5 +8,11 @@ createRoot(document.getElementById('root')!).render(<App />);
 function App() {
 	const socket = useSocket();
 
-	return <>Hello</>;
+	return (
+		<div className='min-h-screen bg-zinc-700 text-white'>
+			<div className='container mx-auto p-2'>
+				<h1 className='text-3xl'>Hello</h1>
+			</div>
+		</div>
+	);
 }
