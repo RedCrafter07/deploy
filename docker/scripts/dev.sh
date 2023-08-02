51a64031b70e33b9b2955156dc68f4dc00babc79
RedCrafter07 <gh@r07.dev>
2023-08-02 13:00:55 +0200

    Fix volume creation

--- docker/scripts/dev.sh
+++ docker/scripts/dev.sh
@@ -1,2 +1,2 @@
-docker volume create --name=reddeploy_config
+docker volume create reddeploy_config
 docker run -p 9272:9272 --rm -v /var/run/docker.sock:/var/run/docker.sock -v reddeploy_config:/data reddeploy/config
