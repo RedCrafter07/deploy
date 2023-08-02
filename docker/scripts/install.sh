b81ac23123708481ee0551c57d07750808725b08
RedCrafter07 <gh@r07.dev>
2023-08-02 11:13:31 +0200

    Add install script

---
+++ docker/scripts/install.sh
@@ -0,0 +1,2 @@
+sudo docker volume create --name=reddeploy_config
+sudo docker run -p 9272:9272 --rm -v /var/run/docker.sock:/var/run/docker.sock -v reddeploy_config:/data ghcr.io/redcrafter07/reddeploy/config
