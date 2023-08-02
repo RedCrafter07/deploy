80552c48fc2824bccdd948c3371768896321db41
RedCrafter07 <gh@r07.dev>
2023-08-02 18:31:11 +0200

    Add build script

---
+++ scripts/dev/buildAll.sh
@@ -0,0 +1,12 @@
+cd packages/
+
+cd config/ && docker build -t reddeploy/config:latest . && cd ..
+cd container-management/ && docker build -t reddeploy/container-management:latest . && cd ..
+cd system-container-management/ && docker build -t reddeploy/system-container-management:latest . && cd ..
+cd web/ && docker build -t reddeploy/web:latest . && cd ..
+
+cd ../
+
+echo "All images built successfully"
+
+exit 0
