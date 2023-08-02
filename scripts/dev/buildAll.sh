86245a38d8205ca705ef1bed6f425eb4bbf451bb
RedCrafter07 <gh@r07.dev>
2023-08-02 18:33:23 +0200

    Change build script

--- scripts/dev/buildAll.sh
+++ scripts/dev/buildAll.sh
@@ -1,6 +1,6 @@
 cd packages/
 
-cd config/ && docker build -t reddeploy/config:latest . && cd ..
+cd config/ && docker build -t reddeploy/config:latest -f Dockerfile.dev . && cd ..
 cd container-management/ && docker build -t reddeploy/container-management:latest . && cd ..
 cd system-container-management/ && docker build -t reddeploy/system-container-management:latest . && cd ..
 cd web/ && docker build -t reddeploy/web:latest . && cd ..
