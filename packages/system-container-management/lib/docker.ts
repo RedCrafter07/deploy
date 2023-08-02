a0f1a3302657c5d75150d4f4e2b6dfebd30b04fa
RedCrafter07 <gh@r07.dev>
2023-08-02 12:55:43 +0200

    Add docker

---
+++ packages/system-container-management/lib/docker.ts
@@ -0,0 +1,42 @@
+import axios from 'axios';
+
+// query socket
+
+type Container = Partial<{
+	Name: string;
+	Image: string;
+	Env: string[];
+	HostConfig: {
+		NetworkMode?: string;
+		Binds?: string[];
+	};
+	Volumes: {
+		[key: string]: {
+			Name: string;
+		};
+	};
+}>;
+
+async function createContainer(container: Container) {
+	const req = await axios({
+		socketPath: '/var/run/docker.sock',
+		method: 'POST',
+		url: '/containers/create',
+		data: container,
+		params: {
+			name: container.Name,
+		},
+	});
+
+	return req.data.Id;
+}
+
+async function startContainer(id: string) {
+	await axios({
+		socketPath: '/var/run/docker.sock',
+		method: 'POST',
+		url: `/containers/${id}/start`,
+	});
+}
+
+export { createContainer, startContainer };
