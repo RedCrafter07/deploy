a5665be16e97cfd15906d7217edd7fc6f8620a32
RedCrafter07 <gh@r07.dev>
2023-08-03 16:01:14 +0200

    Add container name

--- docker/scripts/install.sh
+++ docker/scripts/install.sh
@@ -1,3 +1,3 @@
 docker volume create reddeploy_config
 docker pull ghcr.io/redcrafter07/reddeploy/config
-docker run -p 9272:9272 --rm -v /var/run/docker.sock:/var/run/docker.sock -v reddeploy_config:/data ghcr.io/redcrafter07/reddeploy/config
+docker run -p 9272:9272 --rm -v /var/run/docker.sock:/var/run/docker.sock -v reddeploy_config:/data --name reddeploy_config ghcr.io/redcrafter07/reddeploy/config
