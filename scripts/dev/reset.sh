a00c8c2f6017db6cfbb9e827699db783ec3b565a
RedCrafter07 <gh@r07.dev>
2023-08-03 16:21:41 +0200

    Remove proxy

--- scripts/dev/reset.sh
+++ scripts/dev/reset.sh
@@ -1,5 +1,5 @@
-docker stop reddeploy-mongo reddeploy-cm reddeploy-web reddeploy-scm
-docker rm reddeploy-mongo reddeploy-cm reddeploy-web reddeploy-scm
+docker stop reddeploy-mongo reddeploy-cm reddeploy-web reddeploy-scm reddeploy-proxy
+docker rm reddeploy-mongo reddeploy-cm reddeploy-web reddeploy-scm reddeploy-proxy
 
 docker volume prune -f -a
 
