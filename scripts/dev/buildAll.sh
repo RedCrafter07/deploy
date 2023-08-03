b412bfe21f4309fd580ff448e11c248fcf006c80
RedCrafter07 <gh@r07.dev>
2023-08-03 16:19:44 +0200

    Add proxy to build file

--- scripts/dev/buildAll.sh
+++ scripts/dev/buildAll.sh
@@ -6,6 +6,7 @@ cd config/ && docker build -t reddeploy/config:latest -f Dockerfile.dev . && cd 
 cd container-management/ && docker build -t reddeploy/cm:latest . && cd ..
 cd system-container-management/ && docker build -t reddeploy/scm:latest -f Dockerfile.dev . && cd ..
 cd web/ && docker build -t reddeploy/web:latest . && cd ..
+cd proxy/ && docker build -t reddeploy/proxy:latest . && cd ..
 
 cd ../
 
