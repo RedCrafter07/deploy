7f8e6ce66fcab8af8062c2903698ab810230dbd8
RedCrafter07 <gh@r07.dev>
2023-08-03 13:13:00 +0200

    Add setup proxy route

--- packages/system-container-management/src/client/index.tsx
+++ packages/system-container-management/src/client/index.tsx
@@ -3,6 +3,7 @@ import '@fontsource/figtree';
 import './index.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/home';
+import SetupProxy from './pages/setupProxy';
 
 createRoot(document.getElementById('root')!).render(<App />);
 
@@ -12,6 +13,7 @@ function App() {
 			<Routes>
 				<Route path='/'>
 					<Route index element={<Home />} />
+					<Route path='setup-proxy' element={<SetupProxy />} />
 				</Route>
 			</Routes>
 		</BrowserRouter>
