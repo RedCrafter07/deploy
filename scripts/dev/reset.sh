938128b92ff23b9345f0e9394c902e3f62ea9569
RedCrafter07 <gh@r07.dev>
2023-08-03 15:19:35 +0200

    Fix volume prune

--- scripts/dev/reset.sh
+++ scripts/dev/reset.sh
@@ -1,6 +1,6 @@
 docker stop reddeploy-mongo reddeploy-cm reddeploy-web reddeploy-scm
 docker rm reddeploy-mongo reddeploy-cm reddeploy-web reddeploy-scm
 
-docker volume prune -f
+docker volume prune -f -a
 
 docker network prune -f
