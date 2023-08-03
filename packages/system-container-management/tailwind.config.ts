a80c5df0adcb27e10509d1bae360539afdeb8a7b
RedCrafter07 <gh@r07.dev>
2023-08-03 13:35:50 +0200

    Fix tailwind

--- packages/system-container-management/tailwind.config.ts
+++ packages/system-container-management/tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from 'tailwindcss';
 
 export default {
-	content: ['./src/client/*.{html,js,ts,jsx,tsx}'],
+	content: ['./src/client/**/*.{html,js,ts,jsx,tsx}'],
 	theme: {
 		extend: {},
 	},
