9be6d4ae4597146dc463c077f5266f196e97be5d
RedCrafter07 <gh@r07.dev>
2023-08-04 21:53:23 +0200

    Add clone functions

--- packages/container-management/src/index.ts
+++ packages/container-management/src/index.ts
@@ -1,6 +1,13 @@
 import { Server } from 'socket.io';
 import { io as SocketClient } from 'socket.io-client';
 import { MongoClient } from 'mongodb';
+import { simpleGit } from 'simple-git';
+import { URLSearchParams } from 'url';
+
+const git = simpleGit({
+	baseDir: '/tmp',
+	binary: 'git',
+});
 
 const proxySocket = SocketClient('http://reddeploy-proxy:3000', {
 	autoConnect: true,
@@ -39,3 +46,27 @@ const mongo = new MongoClient(mongoURI, {
 	io.listen(parseInt(process.env.CM_PORT!));
 	console.log('CM socket listening');
 })();
+
+async function cloneGithub(repo: string, username: string, token: string) {
+	const encodedToken = encodeURIComponent(token);
+	const gitUrl = `https://${username}:${encodedToken}@github.com${repo}`;
+
+	const dirname = `/tmp/${generateToken(20)}`;
+
+	await git.clone(gitUrl, dirname);
+
+	return dirname;
+}
+
+function generateToken(length: number) {
+	const chars =
+		'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+	let token = '';
+
+	for (let i = 0; i < length; i++) {
+		token += chars[Math.floor(Math.random() * chars.length)];
+	}
+
+	return token;
+}
