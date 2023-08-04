d9961f3563a0170276b97bc0f52ead5664df4473
RedCrafter07 <gh@r07.dev>
2023-08-04 12:51:33 +0200

    Register add event

--- packages/container-management/src/index.ts
+++ packages/container-management/src/index.ts
@@ -1,11 +1,14 @@
 import { Server } from 'socket.io';
+import { io as SocketClient } from 'socket.io-client';
+
+const ProxySocket = SocketClient('http://reddeploy-proxy:3000');
 
 const io = new Server({
 	transports: ['websocket'],
 });
 
 io.on('connect', (socket) => {
-	console.log('A socket connected!');
+	socket.on('add', (name: string, port: string, domain: string) => {});
 });
 
 io.listen(8080);
