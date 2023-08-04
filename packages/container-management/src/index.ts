2cf856c152c0649e6f624b4fd8d91819ecbe6a4d
RedCrafter07 <gh@r07.dev>
2023-08-04 21:23:46 +0200

    Add database connection

--- packages/container-management/src/index.ts
+++ packages/container-management/src/index.ts
@@ -1,20 +1,41 @@
 import { Server } from 'socket.io';
 import { io as SocketClient } from 'socket.io-client';
+import { MongoClient } from 'mongodb';
 
-const proxySocket = SocketClient('http://reddeploy-proxy:3000');
-
-const io = new Server({
-	transports: ['websocket'],
+const proxySocket = SocketClient('http://reddeploy-proxy:3000', {
+	autoConnect: true,
+	reconnection: true,
 });
 
-io.on('connect', (socket) => {
-	socket.on(
-		'add',
-		(data: { ip: string; port: string; domain: string; name: string }) => {
-			proxySocket.emit('addProject', data.ip, data.port, data.domain);
-		},
-	);
+const mongoURI = `mongodb://${process.env.CM_MONGO_HOST}:${process.env.CM_MONGO_PORT}`;
+
+const mongo = new MongoClient(mongoURI, {
+	auth: {
+		username: process.env.CM_MONGO_USER!,
+		password: process.env.CM_MONGO_PASS!,
+	},
 });
 
-io.listen(8080);
-console.log('CM socket listening');
+(async () => {
+	await mongo.connect();
+
+	console.log('Connected to database!');
+
+	const io = new Server({
+		transports: ['websocket'],
+	});
+
+	io.on('connect', (socket) => {
+		socket.on(
+			'add',
+			(data: { ip: string; port: string; domain: string; name: string }) => {
+				proxySocket.emit('addProject', data.ip, data.port, data.domain);
+
+				socket.emit('add', data);
+			},
+		);
+	});
+
+	io.listen(parseInt(process.env.CM_PORT!));
+	console.log('CM socket listening');
+})();
