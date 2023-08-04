d7c644537bc934677caeb861326568d7c93269ca
RedCrafter07 <gh@r07.dev>
2023-08-04 22:29:11 +0200

    Fix configs

--- packages/web/webpack.config.ts
+++ packages/web/webpack.config.ts
@@ -1,4 +1,54 @@
-import client from './webpack.config.client';
-import server from './webpack.config.server';
+import { Configuration } from 'webpack';
+import type { Swcrc } from '@swc/core';
 
-export default [client, server];
+export default {
+	entry: {
+		client: './src/client/index.tsx',
+	},
+
+	output: {
+		filename: '[name].js',
+		path: __dirname + '/dist/client',
+		publicPath: '/.rd/',
+	},
+
+	resolve: {
+		extensions: ['.ts', '.tsx', '.js', '.jsx', '.css'],
+	},
+
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				loader: 'swc-loader',
+				options: {
+					jsc: {
+						parser: {
+							syntax: 'typescript',
+							jsx: true,
+							decorators: true,
+							dynamicImport: true,
+						},
+						transform: {
+							legacyDecorator: true,
+							react: {
+								pragma: 'React.createElement',
+								pragmaFrag: 'React.Fragment',
+								throwIfNamespace: true,
+								useBuiltins: false,
+							},
+						},
+					},
+				} as Swcrc,
+			},
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader', 'postcss-loader'],
+			},
+			{
+				test: /\.(png|jpg|gif|svg|eot|ttf)$/,
+				loader: 'url-loader',
+			},
+		],
+	},
+} as Configuration;
