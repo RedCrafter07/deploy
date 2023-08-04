b3aa158ed9652f145775ee3e83e3d2cc0722ae07
RedCrafter07 <gh@r07.dev>
2023-08-04 21:16:17 +0200

    Add to proxy

--- packages/container-management/src/index.ts
+++ packages/container-management/src/index.ts
@@ -1,14 +1,19 @@
 import { Server } from 'socket.io';
 import { io as SocketClient } from 'socket.io-client';
 
-const ProxySocket = SocketClient('http://reddeploy-proxy:3000');
+const proxySocket = SocketClient('http://reddeploy-proxy:3000');
 
 const io = new Server({
 	transports: ['websocket'],
 });
 
 io.on('connect', (socket) => {
-	socket.on('add', (name: string, port: string, domain: string) => {});
+	socket.on(
+		'add',
+		(data: { ip: string; port: string; domain: string; name: string }) => {
+			proxySocket.emit('addProject', data.ip, data.port, data.domain);
+		},
+	);
 });
 
 io.listen(8080);
