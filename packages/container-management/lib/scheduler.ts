32e4ab395915266608f2e4f24ed7e3f74a48b55a
RedCrafter07 <gh@r07.dev>
2023-08-05 22:05:47 +0200

    Complete scheduler

--- packages/container-management/lib/scheduler.ts
+++ packages/container-management/lib/scheduler.ts
@@ -1,3 +1,27 @@
-class Scheduler {}
+class Scheduler<T> {
+	private readonly tasks: T[];
+
+	constructor(tasks: T[]) {
+		this.tasks = tasks;
+	}
+
+	addTask(task: T) {
+		this.tasks.push(task);
+
+		if (this.tasks.length === 1) {
+			this.run();
+		}
+	}
+
+	private async run() {
+		while (this.tasks.length > 0) {
+			const task = this.tasks[0];
+
+			await task;
+
+			this.tasks.shift();
+		}
+	}
+}
 
 export default Scheduler;
