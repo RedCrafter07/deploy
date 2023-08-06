b70743d54035658b0d5f63c590818d72f87fb7db
RedCrafter07 <gh@r07.dev>
2023-08-06 18:55:32 +0200

    Lowercase repo in build

--- packages/container-management/src/index.ts
+++ packages/container-management/src/index.ts
@@ -267,7 +267,7 @@ async function buildGithub(
 	token: string,
 ) {
 	const encodedToken = encodeURIComponent(token);
-	const gitUrl = `https://${username}:${encodedToken}@github.com/${repo}.git#${branch}`;
+	const gitUrl = `https://${username}:${encodedToken}@github.com/${repo.toLowerCase()}.git#${branch}`;
 	const imageName = `rd-${repo.replace('/', '-').toLowerCase()}:latest`;
 
 	await buildImage({
