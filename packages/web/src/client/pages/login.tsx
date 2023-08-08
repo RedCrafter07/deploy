ed8e0613939534452970985ee9976fc6614c978d
RedCrafter07 <gh@r07.dev>
2023-08-08 17:12:04 +0200

    Add further styling

--- packages/web/src/client/pages/login.tsx
+++ packages/web/src/client/pages/login.tsx
@@ -2,15 +2,20 @@ export default function Login() {
 	return (
 		<div className='min-h-screen bg-zinc-900 text-zinc-50'>
 			<div className='container mx-auto p-2'>
-				<h1>Login</h1>
-				<p>Please log in to RedDeploy.</p>
+				<h1 className='text-3xl'>Login</h1>
+				<h3 className='text-xl'>Please log in to RedDeploy.</h3>
 
 				<form>
 					<label htmlFor='username'>Username</label>
-					<input type='text' name='username' id='username' />
+					<input className='input' type='text' name='username' id='username' />
 
 					<label htmlFor='password'>Password</label>
-					<input type='password' name='password' id='password' />
+					<input
+						className='input'
+						type='password'
+						name='password'
+						id='password'
+					/>
 				</form>
 			</div>
 		</div>
