5113d2359621f49408c26f8e9846753c5a5dc8a5
RedCrafter07 <gh@r07.dev>
2023-08-08 16:59:49 +0200

    Start login page styling

--- packages/web/src/client/pages/login.tsx
+++ packages/web/src/client/pages/login.tsx
@@ -1,3 +1,18 @@
 export default function Login() {
-	return <></>;
+	return (
+		<div className='min-h-screen bg-zinc-900 text-zinc-50'>
+			<div className='container mx-auto p-2'>
+				<h1>Login</h1>
+				<p>Please log in to RedDeploy.</p>
+
+				<form>
+					<label htmlFor='username'>Username</label>
+					<input type='text' name='username' id='username' />
+
+					<label htmlFor='password'>Password</label>
+					<input type='password' name='password' id='password' />
+				</form>
+			</div>
+		</div>
+	);
 }
