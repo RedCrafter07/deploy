92578aa7c86ac9ccfb84ad3129e28ef839acd1cc
RedCrafter07 <gh@r07.dev>
2023-08-08 17:03:43 +0200

    Import figtree & styling

--- packages/web/src/client/index.tsx
+++ packages/web/src/client/index.tsx
@@ -1,4 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import Router from './core/router';
+import '@fontsource-variable/figtree';
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(<Router />);
