034323eb3c946bf784495a1b5ce141bb8bd46fc1
RedCrafter07 <gh@r07.dev>
2023-08-08 17:59:49 +0200

    Change home page

--- packages/web/src/client/pages/login.tsx
+++ packages/web/src/client/pages/login.tsx
@@ -8,42 +8,59 @@ export default function Login() {
 				<h1 className='text-3xl'>Login</h1>
 				<h3 className='text-xl'>Please log in to RedDeploy.</h3>
 
-				<form
-					onSubmit={(e) => {
-						e.preventDefault();
+				<div className='grid place-items-center'>
+					<div className='p-8 rounded-lg bg-zinc-700 border-2 border-zinc-800'>
+						<form
+							onSubmit={(e) => {
+								console.log('submitting!');
+								e.preventDefault();
 
-						const data = new FormData(e.target as HTMLFormElement);
+								const data = new FormData(e.target as HTMLFormElement);
 
-						axios
-							.post('/auth/login', {
-								username: data.get('username'),
-								password: data.get('password'),
-							})
-							.then(() => {
-								const socket = io(window.location.origin, {
-									reconnection: true,
-									transports: ['websocket'],
-								});
+								axios
+									.post('/auth/login', {
+										username: data.get('username'),
+										password: data.get('password'),
+									})
+									.then(() => {
+										const socket = io(window.location.origin, {
+											reconnection: true,
+											transports: ['websocket'],
+										});
 
-								socket.on('user', (user) => {
-									console.log(user);
-								});
+										socket.on('user', (user) => {
+											console.log(user);
+										});
 
-								socket.connect();
-							});
-					}}
-				>
-					<label htmlFor='username'>Username</label>
-					<input className='input' type='text' name='username' id='username' />
+										socket.connect();
+									});
+							}}
+						>
+							<label htmlFor='username'>Username</label>
+							<input
+								className='input'
+								type='text'
+								name='username'
+								id='username'
+							/>
 
-					<label htmlFor='password'>Password</label>
-					<input
-						className='input'
-						type='password'
-						name='password'
-						id='password'
-					/>
-				</form>
+							<label htmlFor='password'>Password</label>
+							<input
+								className='input'
+								type='password'
+								name='password'
+								id='password'
+							/>
+
+							<button
+								className='w-full bg-zinc-800 rounded-lg text-center p-2 hover:bg-green-600 active:scale-95'
+								type='submit'
+							>
+								Login
+							</button>
+						</form>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
