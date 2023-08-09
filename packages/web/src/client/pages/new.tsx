1aff81d84fe48b57709116c12d6f25a08de19b22
RedCrafter07 <gh@r07.dev>
2023-08-09 12:02:39 +0200

    Add project creation form

--- packages/web/src/client/pages/new.tsx
+++ packages/web/src/client/pages/new.tsx
@@ -1,9 +1,12 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import useSocket from '../util/useSocket';
 import Switch from '../components/Switch';
+import { AnimatePresence, motion } from 'framer-motion';
 
 export default function New() {
 	const socket = useSocket();
+
+	const [withProxy, setWithProxy] = useState(false);
 
 	useEffect(() => {
 		socket.connect();
@@ -19,7 +22,106 @@ export default function New() {
 						e.preventDefault();
 					}}
 				>
-					<Switch label='Enable proxy' defaultChecked name='proxy' />
+					<div className='flex flex-col gap-2'>
+						<label htmlFor='name'>Project name</label>
+						<input type='text' name='name' id='name' className='input' />
+
+						<label htmlFor='repo'>GitHub Repo</label>
+						<input
+							type='text'
+							name='repo'
+							id='repo'
+							className='input'
+							placeholder='username/cool-project'
+						/>
+
+						<label htmlFor='branch'>Branch</label>
+						<input
+							type='text'
+							name='branch'
+							id='branch'
+							className='input'
+							defaultValue='main'
+						/>
+
+						<label htmlFor='username'>GitHub Username</label>
+						<input
+							type='text'
+							name='username'
+							id='username'
+							className='input'
+						/>
+
+						<label htmlFor='token'>GitHub Token</label>
+						<p className='opacity-75'>
+							Info: The token should at least have Read Access to a repository.
+						</p>
+						<p>
+							See{' '}
+							<a
+								target='_blank'
+								href='https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/managing-your-personal-access-tokens#creating-a-fine-grained-personal-access-token'
+								className='underline hover:text-zinc-300'
+							>
+								GitHub's docs
+							</a>{' '}
+							for more info on creating a token.
+						</p>
+						<input type='password' className='input' name='token' id='token' />
+
+						<Switch
+							label='Store password in database'
+							name='store'
+							id='storeToken'
+						/>
+						<p>
+							Info: The token will be stored unencrypted in both your account
+							and the project, however it can only be accessed by the RedDeploy
+							System. If you want to store this token in your user account, this
+							will automatically fill in the token field for you when creating a
+							new project.
+						</p>
+
+						<Switch
+							label='Enable proxy'
+							checked={withProxy}
+							onChange={() => setWithProxy((c) => !c)}
+							name='proxy'
+						/>
+					</div>
+
+					<AnimatePresence>
+						{withProxy && (
+							<motion.div
+								initial={{ opacity: 0 }}
+								animate={{ opacity: 1 }}
+								exit={{ opacity: 0 }}
+								transition={{ duration: 0.2 }}
+							>
+								<div className='flex flex-col gap-2 mt-2'>
+									<label htmlFor='proxyPort'>Port</label>
+									<input
+										type='number'
+										name='port'
+										id='proxyPort'
+										className='input'
+									/>
+
+									<label htmlFor='domain'>Domain</label>
+
+									<div className='flex flex-row gap-2'>
+										<input
+											type='text'
+											name='domain'
+											id='domain'
+											className='input'
+											placeholder='your-project.example.com'
+										/>
+									</div>
+								</div>
+							</motion.div>
+						)}
+					</AnimatePresence>
 				</form>
 			</div>
 		</div>
