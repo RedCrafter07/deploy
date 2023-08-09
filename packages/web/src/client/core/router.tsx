2544c172d26a8dfb21e3a380912fafa4bf5b02a5
RedCrafter07 <gh@r07.dev>
2023-08-09 09:11:09 +0200

    Add dash element

--- packages/web/src/client/core/router.tsx
+++ packages/web/src/client/core/router.tsx
@@ -2,6 +2,7 @@ import { lazy } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const Login = lazy(() => import('../pages/login'));
+const Dash = lazy(() => import('../pages/dash'));
 
 export default function Router() {
 	return (
@@ -16,6 +17,7 @@ function Paths() {
 		<Routes>
 			<Route path='/'>
 				<Route path='login' element={<Login />} />
+				<Route index element={<Dash />} />
 			</Route>
 		</Routes>
 	);
